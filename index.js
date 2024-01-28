e65c47ee47cb851fa18788e63df24712dc9cd343
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-01-27 22:19:03 -0600

    inquirer fully setup

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -45,9 +39,12 @@ var startProgram = function () {
                         return;
                     }
                     console.table(results);
+
+                    console.log('View All Employees clicked!');
+
+                    pressEnter(startProgram);
                 })
 
-                console.log('View All Employees clicked!');
 
             } else if (answers.what === 'View All Roles') {
                 
@@ -57,9 +54,12 @@ var startProgram = function () {
                         return;
                     }
                     console.table(results);
+
+                    console.log('View All Roles clicked!');
+
+                    pressEnter(startProgram);
                 })
 
-                console.log('View All Roles clicked!');
                 
             } else if (answers.what === 'View All Departments') {
 
@@ -74,10 +74,8 @@ var startProgram = function () {
                     console.log('View All Departments clicked!');
 
                     pressEnter(startProgram);
-
-
                 });
-
+                
             } else if (answers.what === 'Add Employee') {
                 console.log('Add Employee clicked!');        
                 
@@ -87,61 +85,175 @@ var startProgram = function () {
                         return;
                     }
 
-                    inquirer
-                    .prompt ([
-                        {
-                            type: 'input',
-                            name: 'first_name',
-                            message: "What is the employee's first name?",
-                            validate: function (input) {
-                                if (input.trim() === '') {
-                                    return 'Please enter a first name.';
-                                }
-                                return true; 
-                            }
-                        },
-                        {
-                            type: 'input',
-                            name: 'last_name',
-                            message: "What is the employee's last name?",
-                            validate: function (input) {
-                                if (input.trim() === '') {
-                                    return 'Please enter a last name.';
-                                }
-                                return true;
-                            }
-                        },
-                        {
-                            type: 'list',
-                            name: 'employee_role',
-                            message: "What is the employee's role?",
-                            choices: results.map(role => role.title)
+                    db.query('SELECT CONCAT(first_name, " ", last_name) AS manager_name FROM employee', (managerError, managerResults, managerFields) => {
+                        if (managerError) {
+                            console.error('Error fetching employees from the employee table:', managerError);
+                            return;
                         }
-                    ])
-                    .then((answers) => {
-                        console.log(answers.first_name, answers.last_name, answers.employee_role);
 
-                    })
+                        inquirer
+                        .prompt ([
+                            {
+                                type: 'input',
+                                name: 'first_name',
+                                message: "What is the employee's first name?",
+                                validate: function (input) {
+                                    if (input.trim() === '') {
+                                        return 'Please enter a first name.';
+                                    }
+                                    return true; 
+                                }
+                            },
+                            {
+                                type: 'input',
+                                name: 'last_name',
+                                message: "What is the employee's last name?",
+                                validate: function (input) {
+                                    if (input.trim() === '') {
+                                        return 'Please enter a last name.';
+                                    }
+                                    return true;
+                                }
+                            },
+                            {
+                                type: 'list',
+                                name: 'employee_role',
+                                message: "What is the employee's role?",
+                                choices: results.map(role => role.title)
+                            },
+                            {
+                                type: 'list',
+                                name: 'employee_manager',
+                                message: "Who is the employee's manager?",
+                                choices: managerResults.map(employee => employee.manager_name)
+                            }
+                        ])
+                        .then((answers) => {
+                            //NEED TO ADD TO THE DATABASE
+                            console.log(`Added ${answers.first_name} ${answers.last_name} to the database.`);    
+
+                            pressEnter(startProgram);
+                        })
+                        .catch((error) => {
+
+                        });
+
+                    });
 
                 });
-                
-                
-        
-
+    
             } else if (answers.what === 'Update Employee Role') {
                 console.log('Update Employee Role clicked!');
 
+                db.query('SELECT CONCAT(first_name, " ", last_name) AS employee_name FROM employee', (error, results, fields) => {
+                    if (error) {
+                        console.error('Error fetching employees from the employee table:', error);
+                        return;
+                    }
+
+                    db.query('SELECT title FROM role', (roleError, roleResults, roleFields) => {
+                        if(roleError) {
+                            console.error('Error fetching roles from the role table:', roleError);
+                            return;
+                        }
+
+                        inquirer
+                        .prompt([
+                            {
+                                type: 'list',
+                                name: 'employeeList',
+                                message: "Which employee's role do you want to update?",
+                                choices: results.map(employee => employee.employee_name)
+                            },
+                            {
+                                type: 'list',
+                                name: 'roleList',
+                                message: 'Which role do you want to assign the selected employee?',
+                                choices: roleResults.map(role => role.title)
+                            }
+                        ])
+                        .then((answers) => {
+                            //NEED TO UPDATE THE DATABASE
+                            console.log(`Updated employee's role.`);    
+
+                            pressEnter(startProgram);
+                        })
+                        .catch((error) => {
+
+                        });
+                    });
+                });
+
 
             } else if (answers.what === 'Add Role') {
+
                 console.log('Add Role clicked!');
+
+                db.query('SELECT name FROM department', (error, results, fields) => {
+                    if (error) {
+                        console.error('Error fetch department names from the department table:', error);
+                        return;
+                    }
+
+                    inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'roleName',
+                            message: 'What is the name of the role?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'roleSalary',
+                            message: 'What is the salary of the role?'
+                        },
+                        {
+                            type: 'list',
+                            name: 'roleDepartment',
+                            message: 'Which department does the role belong to?',
+                            choices: results.map(department => department.name)
+                        }
+                    ])
+                    .then((answers) => {
+                        //NEED TO ADD TO THE DATABASE
+                        console.log(`Added ${answers.roleName} to the database`);
+                        pressEnter(startProgram);
+                    })
+                    .catch((error) => {
+
+                    });
+                });
 
 
             } else if (answers.what === 'Add Department') {
                 console.log('Add Department clicked!');
 
+                inquirer
+                .prompt ([
+                    {
+                        type: 'input',
+                        name: 'databaseName',
+                        message: 'What is the name of the department?',
+                        validate: function (input) {
+                            if (input.trim() === '') {
+                                return 'Please enter a name for the department.';
+                            }
+                            return true; 
+                        } 
+                    }
+                ])
+                .then((answers) => {
+                    //NEED TO ADD TO THE DATABASE
+                    console.log(`Added ${answers.databaseName} to the database.`);
+                    pressEnter(startProgram);
+                })
+                .catch((error) => {
+
+                });
 
             } else if (answers.what === 'Quit') {
                 console.log('Quit Clicked');
+                process.exit();
             }
         })
         .catch((error) => {
@@ -172,7 +284,7 @@ function pressEnter(callback) {
             process.stdin.removeListener('keypress', listener);
             process.stdin.pause();
             process.stdin.setRawMode(false);
-            console.log('\nContinuing...');
+            console.log('\nContinuing...\n');
             callback();
         }
     });
