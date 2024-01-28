f3606961ca94df3e7d6331444cd3e528a9a846ce
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-01-27 18:13:09 -0600

    .then conditional statements routed, with startProgram(); to route it back to initial question, .then() successfully tested

--- index.js
+++ index.js
@@ -12,7 +12,49 @@ db.connect(err => {
     if (err) throw err;
     console.log('Database connected');
     //function that starts program
+    startProgram();
 })
 
 //function that starts program
 
+var startProgram = function () {
+    // console.log('Program Started!')
+    inquirer
+        .prompt ([
+            {
+                type: 'list',
+                name: 'what',
+                message: 'What would you like to do?',
+                choices: ['View All Employees', 'View All Roles', 'View All Departments', 'Add Employee', 'Update Employee Role', 'Add Role', 'Add Department', 'Quit']
+            }
+        ])
+        .then ((answers) => {
+            if (answers.what === 'View All Employees') {
+                console.log('View All Employees clicked!');
+                startProgram();
+            } else if (answers.what === 'View All Roles') {
+                console.log('View All Roles clicked!');
+                startProgram();
+            } else if (answers.what === 'View All Departments') {
+                console.log('View All Departments clicked!');
+                startProgram();
+            } else if (answers.what === 'Add Employee') {
+                console.log('Add Employee clicked!');                startProgram();
+            } else if (answers.what === 'Update Employee Role') {
+                console.log('Update Employee Role clicked!');
+                startProgram();
+            } else if (answers.what === 'Add Role') {
+                console.log('Add Role clicked!');
+                startProgram();
+            } else if (answers.what === 'Add Department') {
+                console.log('Add Department clicked!');
+                startProgram();
+            } else if (answers.what === 'Quit') {
+                console.log('Quit Clicked');
+                startProgram();
+            }
+        })
+        .catch((error) => {
+
+        })
+}
